c0d0c4492dfd9c6efa6cfce03f6e65e63d6ea47b
TheSweerX <thesweer@gmail.com>
2020-02-20 22:01:43 +0100

    add writing and reading to server an client apps

--- src/client/client.go
+++ src/client/client.go
@@ -10,21 +10,31 @@ import (
 )
 
 func main() {
+
+	expected := ([]byte)("notest")
+
 	str := `{"page": 1, "fruits": ["apple", "peach"]}`
+	fmt.Println(str)
 
 	// connect to socket
 	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
 
 	fmt.Fprintf(conn, str+"\n")
 	message, _ := bufio.NewReader(conn).ReadString('\n')
-	fmt.Print("Message from server: " + message)
+	fmt.Println("Message from server: " + message)
 
 	file, err := shm.Open("myShm", os.O_RDONLY, 0600)
 	defer file.Close()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(str)
+
+	buff := make([]byte, len(expected))
+	if _, err := file.ReadAt(buff, 0); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(buff))
 
 	// for i := 0; true; i++ {
 	// 	time.Sleep(1)
--- src/server/server.go
+++ src/server/server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"strings"
+	"syscall"
 
 	"github.com/fabiokung/shm"
 )
@@ -14,14 +15,32 @@ import (
 
 func main() {
 
+	expected := ([]byte)("a test")
+
 	fmt.Println("Launching server...")
 
 	file, err := shm.Open("myShm", os.O_RDWR|os.O_CREATE, 0600)
+	defer file.Close()
+	defer shm.Unlink(file.Name())
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Print(file)
+	if err := syscall.Ftruncate(
+		int(file.Fd()), int64(len(expected)),
+	); err != nil {
+		panic(err)
+	}
+
+	if _, err := file.Write(expected); err != nil {
+		panic(err)
+	}
+
+	if err := file.Sync(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(file)
 
 	// listen on all interfaces
 	ln, _ := net.Listen("tcp", ":8081")
@@ -32,13 +51,11 @@ func main() {
 	// will listen for message to process ending in newline (\n)
 	message, _ := bufio.NewReader(conn).ReadString('\n')
 	// output message received
-	fmt.Print("Message Received:", string(message))
+	fmt.Println("Message Received:", string(message))
 	// sample process for string received
 	newmessage := strings.ToUpper(message)
 	// send new string back to client
 	conn.Write([]byte(newmessage + "\n"))
 
 	conn.Close()
-	defer file.Close()
-	shm.Unlink(file.Name())
 }
